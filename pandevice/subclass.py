f77eb04929d6e17c0122fa4a5c651dc5ea3f9860
Austin de Coup-Crank <austindcc@gmail.com>
2023-11-15 10:01:46 -0600

    Tinkering with PAN APIs

---
+++ pandevice/subclass.py
@@ -0,0 +1,42 @@
+from panos.panorama import Panorama as OriginalPanorama
+import config
+import ipdb
+import xml.etree.ElementTree as ET
+
+
+class Panorama(OriginalPanorama):
+    def test_security_policy_match(
+        self,
+        source: str,
+        destination: str,
+        protocol: int,
+        application=None,
+        category=None,
+        port=None,
+        user=None,
+        from_zone=None,
+        to_zone=None,
+        show_all=False,
+    ):
+        root = ET.Element("request-batch")
+        op = ET.SubElement(root, "op-command")
+        device = ET.SubElement(op, "device")
+        ET.SubElement(device, "entry", {"name": "016401016351"})
+        test = ET.SubElement(op, "test")
+        policy_match = ET.SubElement(test, "security-policy-match")
+        ET.SubElement(policy_match, "source").text = source
+        ET.SubElement(policy_match, "destination").text = destination
+        ET.SubElement(policy_match, "destination-port").text = str(port)
+        ET.SubElement(policy_match, "protocol").text = str(protocol)
+
+        return self.op(cmd=ET.tostring(root), cmd_xml=False)
+
+
+if __name__ == "__main__":
+    pano = Panorama(
+        hostname=config.HOST, api_username=config.USERNAME, api_password=config.PASSWORD
+    )
+    result = pano.test_security_policy_match(
+        source="10.1.1.1", destination="8.8.8.8", protocol=6, port=80
+    )
+    ipdb.set_trace()
--- pandevice/test.py
+++ pandevice/test.py
@@ -8,14 +8,22 @@ from httpx import URL
 import xml.etree.ElementTree as ET
 import re
 
-tree = ET.parse("sample.xml")
-xml = ET.tostring(tree.getroot(), encoding="utf-8").decode()
-cmd = re.sub(r"\s{2,}+", "", xml).replace("\n", "")
+root = ET.Element("request-batch")
+op = ET.SubElement(root, "op-command")
+device = ET.SubElement(op, "device")
+ET.SubElement(device, "entry", {"name": "016401016351"})
+test = ET.SubElement(op, "test")
+policy_match = ET.SubElement(test, "security-policy-match")
+ET.SubElement(policy_match, "source").text = "10.1.1.1"
+ET.SubElement(policy_match, "destination").text = "8.8.8.8"
+ET.SubElement(policy_match, "destination-port").text = "80"
+ET.SubElement(policy_match, "protocol").text = "6"
+ET.tostring(root)
+ipdb.set_trace()
 
 # cmd = '<request-batch><op-command><device><entry name="016401016351"><vsys><list><<member>vsys1</member></list></vsys></entry></device><test><security-policy-match><source>10.1.1.1</source><destination>1.1.1.1</destination><destination-port>1</destination-port><protocol>1</protocol></security-policy-match></test></op-command></request-batch>'
-response = httpx.get(
-    f"https://{config.HOST}/api/?type=op&cmd={cmd}&key={config.API_KEY}", verify=False
-)
+# response = httpx.get(
+#     f"https://{config.HOST}/api/?type=op&cmd={cmd}&key={config.API_KEY}", verify=False
+# )
 # status = httpx.get(f'https://{config.HOST}/api/?key={config.API_KEY}&type=log&action=get&job-id=')
 # response = httpx.get(f'https://{config.HOST}/api/?type=keygen&user={config.USERNAME}&password={config.PASSWORD}', verify=False)
-ipdb.set_trace()
