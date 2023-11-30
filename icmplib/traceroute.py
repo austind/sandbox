7ec3d8ee657550802f2436d50cb1f1cbe65c5c06
Austin de Coup-Crank <austindcc@gmail.com>
2023-11-30 10:30:07 -0600

    comment

--- icmplib/traceroute.py
+++ icmplib/traceroute.py
@@ -7,6 +7,8 @@ trace = traceroute(config.TRACE_DEST)
 hosts = []
 for hop in trace:
     hop: Hop = hop
+    # gethostbyaddr returns a 3-tuple (hostname, aliaslist, ipaddrlist)
+    # https://docs.python.org/3/library/socket.html#socket.gethostbyaddr
     interface = gethostbyaddr(hop.address)[0]
     host = interface.split("--")[0]
     hosts.append((host, interface, hop.address))
