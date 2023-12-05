8a69e269aa87f2a1061270505f8be23127bf2d5d
Austin de Coup-Crank <austindcc@gmail.com>
2023-12-05 15:54:46 -0600

    Netmiko

---
+++ netmiko/eos_cmd.py
@@ -0,0 +1,10 @@
+import config
+
+from netmiko import ConnectHandler
+
+CMD = f"bash timeout 60 traceroute {config.DEST} -s {config.SOURCE} -m 30 -w 2"
+
+with ConnectHandler(
+    host=config.SOURCE, username=config.USERNAME, password=config.PASSWORD, device_type="arista_eos"
+) as conn:
+    results = conn.send_command(CMD)
